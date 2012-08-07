788c8cebf7158fdd5bb7285f630e67563528d29b
Горлов Максим <gorlov.maxim@gmail.com>
2012-08-07 08:46:39 +0400

    обновление sql

--- sql/schema.sql
+++ sql/schema.sql
@@ -4,7 +4,7 @@
 
 -- Dumped from database version 9.1.4
 -- Dumped by pg_dump version 9.1.4
--- Started on 2012-07-19 18:50:38 MSK
+-- Started on 2012-08-07 08:46:08 MSK
 
 SET statement_timeout = 0;
 SET client_encoding = 'UTF8';
@@ -13,7 +13,7 @@ SET check_function_bodies = false;
 SET client_min_messages = warning;
 
 --
--- TOC entry 2789 (class 1262 OID 16581)
+-- TOC entry 2795 (class 1262 OID 16581)
 -- Name: rsdn-irc-bot; Type: DATABASE; Schema: -; Owner: rsdn
 --
 
@@ -39,7 +39,7 @@ CREATE EXTENSION IF NOT EXISTS plpgsql WITH SCHEMA pg_catalog;
 
 
 --
--- TOC entry 2792 (class 0 OID 0)
+-- TOC entry 2798 (class 0 OID 0)
 -- Dependencies: 171
 -- Name: EXTENSION plpgsql; Type: COMMENT; Schema: -; Owner: 
 --
@@ -50,8 +50,8 @@ COMMENT ON EXTENSION plpgsql IS 'PL/pgSQL procedural language';
 SET search_path = public, pg_catalog;
 
 --
--- TOC entry 183 (class 1255 OID 88823)
--- Dependencies: 5 522
+-- TOC entry 184 (class 1255 OID 88823)
+-- Dependencies: 524 5
 -- Name: update_rsdn_messages(bigint, bigint, bigint, bigint, bigint, character varying, character varying, text, bigint, timestamp with time zone, timestamp with time zone, character varying, character varying, timestamp with time zone, boolean); Type: FUNCTION; Schema: public; Owner: rsdn
 --
 
@@ -82,8 +82,60 @@ $$;
 ALTER FUNCTION public.update_rsdn_messages(i_id bigint, i_topicid bigint, i_parentid bigint, i_userid bigint, i_forumid bigint, i_subject character varying, i_messagename character varying, i_message text, i_articleid bigint, i_messagedate timestamp with time zone, i_updatedate timestamp with time zone, i_userrole character varying, i_usertitle character varying, i_lastmoderated timestamp with time zone, i_closed boolean) OWNER TO rsdn;
 
 --
--- TOC entry 184 (class 1255 OID 88834)
--- Dependencies: 5 522
+-- TOC entry 186 (class 1255 OID 107116)
+-- Dependencies: 5 524
+-- Name: update_rsdn_moderate(timestamp with time zone, bigint, bigint, bigint, bigint); Type: FUNCTION; Schema: public; Owner: rsdn
+--
+
+CREATE FUNCTION update_rsdn_moderate(i_crerate timestamp with time zone, i_messageid bigint, i_topicid bigint, i_userid bigint, i_forumid bigint) RETURNS boolean
+    LANGUAGE plpgsql
+    AS $$
+begin
+  begin
+    INSERT INTO rsdn_moderate(crerate, messageid, topicid, userid, forumid)
+    VALUES(i_crerate, i_messageid, i_topicid, i_userid, i_forumid);
+    EXCEPTION WHEN unique_violation THEN
+        update rsdn_moderate
+        set crerate=i_crerate
+        where messageid=i_messageid and topicid=i_topicid and userid=i_userid and forumid=i_forumid;
+     return true;
+  end;
+  return false;
+end;
+$$;
+
+
+ALTER FUNCTION public.update_rsdn_moderate(i_crerate timestamp with time zone, i_messageid bigint, i_topicid bigint, i_userid bigint, i_forumid bigint) OWNER TO rsdn;
+
+--
+-- TOC entry 183 (class 1255 OID 107113)
+-- Dependencies: 524 5
+-- Name: update_rsdn_rating(integer, integer, timestamp with time zone, bigint, bigint, bigint); Type: FUNCTION; Schema: public; Owner: rsdn
+--
+
+CREATE FUNCTION update_rsdn_rating(i_userrating integer, i_rate integer, i_ratedate timestamp with time zone, i_messageid bigint, i_topicid bigint, i_userid bigint) RETURNS boolean
+    LANGUAGE plpgsql
+    AS $$
+begin
+  begin
+    INSERT INTO rsdn_rating(userrating, rate, ratedate, messageid, topicid, userid)
+    VALUES(i_userrating, i_rate, i_ratedate, i_messageid, i_topicid, i_userid);
+    EXCEPTION WHEN unique_violation THEN
+        update rsdn_rating
+        set userrating=i_userrating, rate=i_rate, ratedate=i_ratedate
+        where messageid=i_messageid and topicid=i_topicid and userid=i_userid;
+     return true;
+  end;
+  return false;
+end;
+$$;
+
+
+ALTER FUNCTION public.update_rsdn_rating(i_userrating integer, i_rate integer, i_ratedate timestamp with time zone, i_messageid bigint, i_topicid bigint, i_userid bigint) OWNER TO rsdn;
+
+--
+-- TOC entry 185 (class 1255 OID 88834)
+-- Dependencies: 524 5
 -- Name: update_rsdn_users(bigint, character varying, character varying, character varying, character varying, character varying, character varying, character varying, integer); Type: FUNCTION; Schema: public; Owner: postgres
 --
 
@@ -92,7 +144,6 @@ CREATE FUNCTION update_rsdn_users(i_id bigint, i_usernick character varying, i_u
     AS $$
 begin
   begin
-
     INSERT INTO rsdn_users(usernick, username, realname, homepage, wherefrom, origin, id, confirmed, specialization, userclass)
     VALUES (i_usernick, i_username, i_realname, i_homepage, i_wherefrom, i_origin, i_id, false, i_specialization, i_userclass);
     EXCEPTION WHEN unique_violation THEN
@@ -115,7 +166,7 @@ SET default_with_oids = false;
 
 --
 -- TOC entry 165 (class 1259 OID 16613)
--- Dependencies: 2764 5
+-- Dependencies: 2766 5
 -- Name: channels_logs; Type: TABLE; Schema: public; Owner: rsdn; Tablespace: 
 --
 
@@ -148,7 +199,7 @@ CREATE SEQUENCE channels_logs_id_seq
 ALTER TABLE public.channels_logs_id_seq OWNER TO rsdn;
 
 --
--- TOC entry 2793 (class 0 OID 0)
+-- TOC entry 2799 (class 0 OID 0)
 -- Dependencies: 164
 -- Name: channels_logs_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: rsdn
 --
@@ -158,7 +209,7 @@ ALTER SEQUENCE channels_logs_id_seq OWNED BY channels_logs.id;
 
 --
 -- TOC entry 163 (class 1259 OID 16601)
--- Dependencies: 2762 5
+-- Dependencies: 2764 5
 -- Name: nicknames; Type: TABLE; Schema: public; Owner: rsdn; Tablespace: 
 --
 
@@ -189,7 +240,7 @@ CREATE SEQUENCE nicknames_id_seq
 ALTER TABLE public.nicknames_id_seq OWNER TO rsdn;
 
 --
--- TOC entry 2794 (class 0 OID 0)
+-- TOC entry 2800 (class 0 OID 0)
 -- Dependencies: 162
 -- Name: nicknames_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: rsdn
 --
@@ -248,9 +299,9 @@ ALTER TABLE public.rsdn_moderate OWNER TO rsdn;
 --
 
 CREATE TABLE rsdn_rating (
-    messageid bigint,
-    topicid bigint,
-    userid bigint,
+    messageid bigint NOT NULL,
+    topicid bigint NOT NULL,
+    userid bigint NOT NULL,
     userrating integer,
     rate integer,
     ratedate timestamp with time zone
@@ -275,7 +326,7 @@ ALTER TABLE public.rsdn_row_versions OWNER TO rsdn;
 
 --
 -- TOC entry 161 (class 1259 OID 16591)
--- Dependencies: 2760 5
+-- Dependencies: 2762 5
 -- Name: rsdn_users; Type: TABLE; Schema: public; Owner: rsdn; Tablespace: 
 --
 
@@ -297,7 +348,7 @@ ALTER TABLE public.rsdn_users OWNER TO rsdn;
 
 --
 -- TOC entry 167 (class 1259 OID 16653)
--- Dependencies: 5 161
+-- Dependencies: 161 5
 -- Name: rsdn_users_id_seq; Type: SEQUENCE; Schema: public; Owner: rsdn
 --
 
@@ -312,7 +363,7 @@ CREATE SEQUENCE rsdn_users_id_seq
 ALTER TABLE public.rsdn_users_id_seq OWNER TO rsdn;
 
 --
--- TOC entry 2795 (class 0 OID 0)
+-- TOC entry 2801 (class 0 OID 0)
 -- Dependencies: 167
 -- Name: rsdn_users_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: rsdn
 --
@@ -321,8 +372,8 @@ ALTER SEQUENCE rsdn_users_id_seq OWNED BY rsdn_users.id;
 
 
 --
--- TOC entry 2763 (class 2604 OID 16616)
--- Dependencies: 164 165 165
+-- TOC entry 2765 (class 2604 OID 16616)
+-- Dependencies: 165 164 165
 -- Name: id; Type: DEFAULT; Schema: public; Owner: rsdn
 --
 
@@ -330,8 +381,8 @@ ALTER TABLE ONLY channels_logs ALTER COLUMN id SET DEFAULT nextval('channels_log
 
 
 --
--- TOC entry 2761 (class 2604 OID 16604)
--- Dependencies: 163 162 163
+-- TOC entry 2763 (class 2604 OID 16604)
+-- Dependencies: 162 163 163
 -- Name: id; Type: DEFAULT; Schema: public; Owner: rsdn
 --
 
@@ -339,7 +390,7 @@ ALTER TABLE ONLY nicknames ALTER COLUMN id SET DEFAULT nextval('nicknames_id_seq
 
 
 --
--- TOC entry 2759 (class 2604 OID 16655)
+-- TOC entry 2761 (class 2604 OID 16655)
 -- Dependencies: 167 161
 -- Name: id; Type: DEFAULT; Schema: public; Owner: rsdn
 --
@@ -348,7 +399,7 @@ ALTER TABLE ONLY rsdn_users ALTER COLUMN id SET DEFAULT nextval('rsdn_users_id_s
 
 
 --
--- TOC entry 2777 (class 2606 OID 16622)
+-- TOC entry 2779 (class 2606 OID 16622)
 -- Dependencies: 165 165
 -- Name: pkey_channels_logs; Type: CONSTRAINT; Schema: public; Owner: rsdn; Tablespace: 
 --
@@ -358,7 +409,7 @@ ALTER TABLE ONLY channels_logs
 
 
 --
--- TOC entry 2773 (class 2606 OID 20524)
+-- TOC entry 2775 (class 2606 OID 20524)
 -- Dependencies: 163 163
 -- Name: pkey_nicknames; Type: CONSTRAINT; Schema: public; Owner: rsdn; Tablespace: 
 --
@@ -368,7 +419,7 @@ ALTER TABLE ONLY nicknames
 
 
 --
--- TOC entry 2783 (class 2606 OID 16671)
+-- TOC entry 2785 (class 2606 OID 16671)
 -- Dependencies: 168 168
 -- Name: pkey_rsdn_messages; Type: CONSTRAINT; Schema: public; Owner: rsdn; Tablespace: 
 --
@@ -378,7 +429,27 @@ ALTER TABLE ONLY rsdn_messages
 
 
 --
--- TOC entry 2769 (class 2606 OID 16663)
+-- TOC entry 2792 (class 2606 OID 107115)
+-- Dependencies: 170 170 170 170 170
+-- Name: pkey_rsdn_moderate; Type: CONSTRAINT; Schema: public; Owner: rsdn; Tablespace: 
+--
+
+ALTER TABLE ONLY rsdn_moderate
+    ADD CONSTRAINT pkey_rsdn_moderate PRIMARY KEY (messageid, topicid, userid, forumid);
+
+
+--
+-- TOC entry 2790 (class 2606 OID 107111)
+-- Dependencies: 169 169 169 169
+-- Name: pkey_rsdn_rating; Type: CONSTRAINT; Schema: public; Owner: rsdn; Tablespace: 
+--
+
+ALTER TABLE ONLY rsdn_rating
+    ADD CONSTRAINT pkey_rsdn_rating PRIMARY KEY (messageid, topicid, userid);
+
+
+--
+-- TOC entry 2771 (class 2606 OID 16663)
 -- Dependencies: 161 161
 -- Name: pkey_rsdn_users; Type: CONSTRAINT; Schema: public; Owner: rsdn; Tablespace: 
 --
@@ -388,7 +459,7 @@ ALTER TABLE ONLY rsdn_users
 
 
 --
--- TOC entry 2779 (class 2606 OID 16634)
+-- TOC entry 2781 (class 2606 OID 16634)
 -- Dependencies: 166 166
 -- Name: pkey_rsdn_versions; Type: CONSTRAINT; Schema: public; Owner: rsdn; Tablespace: 
 --
@@ -398,7 +469,7 @@ ALTER TABLE ONLY rsdn_row_versions
 
 
 --
--- TOC entry 2774 (class 1259 OID 20520)
+-- TOC entry 2776 (class 1259 OID 20520)
 -- Dependencies: 165
 -- Name: i_channels_logs_channel; Type: INDEX; Schema: public; Owner: rsdn; Tablespace: 
 --
@@ -407,7 +478,7 @@ CREATE INDEX i_channels_logs_channel ON channels_logs USING btree (channel);
 
 
 --
--- TOC entry 2775 (class 1259 OID 20519)
+-- TOC entry 2777 (class 1259 OID 20519)
 -- Dependencies: 165
 -- Name: i_channels_logs_nick; Type: INDEX; Schema: public; Owner: rsdn; Tablespace: 
 --
@@ -416,7 +487,7 @@ CREATE INDEX i_channels_logs_nick ON channels_logs USING btree (nickname_id);
 
 
 --
--- TOC entry 2770 (class 1259 OID 20525)
+-- TOC entry 2772 (class 1259 OID 20525)
 -- Dependencies: 163
 -- Name: i_nicknames_nickname; Type: INDEX; Schema: public; Owner: rsdn; Tablespace: 
 --
@@ -425,7 +496,7 @@ CREATE INDEX i_nicknames_nickname ON nicknames USING btree (nickname);
 
 
 --
--- TOC entry 2771 (class 1259 OID 20526)
+-- TOC entry 2773 (class 1259 OID 20526)
 -- Dependencies: 163
 -- Name: i_nicknames_rsdn; Type: INDEX; Schema: public; Owner: rsdn; Tablespace: 
 --
@@ -434,7 +505,7 @@ CREATE INDEX i_nicknames_rsdn ON nicknames USING btree (rsdn_user_id);
 
 
 --
--- TOC entry 2780 (class 1259 OID 20472)
+-- TOC entry 2782 (class 1259 OID 20472)
 -- Dependencies: 168
 -- Name: i_rsdn_messages_parentid; Type: INDEX; Schema: public; Owner: rsdn; Tablespace: 
 --
@@ -443,7 +514,7 @@ CREATE INDEX i_rsdn_messages_parentid ON rsdn_messages USING btree (parentid);
 
 
 --
--- TOC entry 2781 (class 1259 OID 20471)
+-- TOC entry 2783 (class 1259 OID 20471)
 -- Dependencies: 168
 -- Name: i_rsdn_messages_userid; Type: INDEX; Schema: public; Owner: rsdn; Tablespace: 
 --
@@ -452,7 +523,7 @@ CREATE INDEX i_rsdn_messages_userid ON rsdn_messages USING btree (userid);
 
 
 --
--- TOC entry 2784 (class 1259 OID 20479)
+-- TOC entry 2786 (class 1259 OID 20479)
 -- Dependencies: 169 169 169
 -- Name: i_rsdn_rating_ids; Type: INDEX; Schema: public; Owner: rsdn; Tablespace: 
 --
@@ -461,7 +532,7 @@ CREATE INDEX i_rsdn_rating_ids ON rsdn_rating USING btree (messageid, topicid, u
 
 
 --
--- TOC entry 2785 (class 1259 OID 61692)
+-- TOC entry 2787 (class 1259 OID 61692)
 -- Dependencies: 169
 -- Name: i_rsdn_rating_messageid; Type: INDEX; Schema: public; Owner: rsdn; Tablespace: 
 --
@@ -470,7 +541,7 @@ CREATE INDEX i_rsdn_rating_messageid ON rsdn_rating USING btree (messageid);
 
 
 --
--- TOC entry 2786 (class 1259 OID 61649)
+-- TOC entry 2788 (class 1259 OID 61649)
 -- Dependencies: 169
 -- Name: i_rsdn_rating_userid; Type: INDEX; Schema: public; Owner: rsdn; Tablespace: 
 --
@@ -479,7 +550,7 @@ CREATE INDEX i_rsdn_rating_userid ON rsdn_rating USING btree (userid);
 
 
 --
--- TOC entry 2765 (class 1259 OID 20505)
+-- TOC entry 2767 (class 1259 OID 20505)
 -- Dependencies: 161
 -- Name: i_rsdn_users_realname; Type: INDEX; Schema: public; Owner: rsdn; Tablespace: 
 --
@@ -488,7 +559,7 @@ CREATE INDEX i_rsdn_users_realname ON rsdn_users USING btree (realname);
 
 
 --
--- TOC entry 2766 (class 1259 OID 20503)
+-- TOC entry 2768 (class 1259 OID 20503)
 -- Dependencies: 161
 -- Name: i_rsdn_users_username; Type: INDEX; Schema: public; Owner: rsdn; Tablespace: 
 --
@@ -497,7 +568,7 @@ CREATE INDEX i_rsdn_users_username ON rsdn_users USING btree (username);
 
 
 --
--- TOC entry 2767 (class 1259 OID 20504)
+-- TOC entry 2769 (class 1259 OID 20504)
 -- Dependencies: 161
 -- Name: i_rsdn_users_usernick; Type: INDEX; Schema: public; Owner: rsdn; Tablespace: 
 --
@@ -506,7 +577,7 @@ CREATE INDEX i_rsdn_users_usernick ON rsdn_users USING btree (usernick);
 
 
 --
--- TOC entry 2791 (class 0 OID 0)
+-- TOC entry 2797 (class 0 OID 0)
 -- Dependencies: 5
 -- Name: public; Type: ACL; Schema: -; Owner: postgres
 --
@@ -517,7 +588,7 @@ GRANT ALL ON SCHEMA public TO postgres;
 GRANT ALL ON SCHEMA public TO PUBLIC;
 
 
--- Completed on 2012-07-19 18:50:41 MSK
+-- Completed on 2012-08-07 08:46:08 MSK
 
 --
 -- PostgreSQL database dump complete
